5c50b135336c24366912c9f3ddea4d623b94ae3e
a-ruv <a.ruv009@gmail.com>
2022-08-19 18:24:40 -0400

    2nd commit

---
+++ src/main/java/com/javaproject/aaj/weekendwalkers/WeekendWalkersApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.aaj.weekendwalkers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekendWalkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekendWalkersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaproject/aaj/weekendwalkers/WeekendWalkersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.aaj.weekendwalkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekendWalkersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
