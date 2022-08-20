00a1ae16a65edf70a5d193e4749c05bfcce2e956
a-ruv <a.ruv009@gmail.com>
2022-08-20 11:15:53 -0400

    Event model and Repo

---
+++ src/main/java/com/javaproject/aaj/weekendwalkers/models/Event.java
@@ -0,0 +1,121 @@
+package com.javaproject.aaj.weekendwalkers.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="events")
+public class Event {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(min=1)
+	private String name;
+	
+	@NotNull
+	@DateTimeFormat(pattern="MM-dd-yyyy")
+	private Date date;
+	
+	@NotNull
+	@DateTimeFormat(pattern="HH:mm")
+	private Date time;
+	
+	@NotNull
+	@Size(min=1)
+	private String location;
+	
+	protected void whenCreated() {
+		this.date = new Date();
+		this.time = new Date();
+	}
+	
+	@Column(updatable=false)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createdAt;
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date updatedAt;
+	  
+	@PrePersist
+	protected void onCreate(){
+		this.createdAt = new Date();
+	}
+	    
+	@PreUpdate
+	protected void onUpdate(){
+		this.updatedAt = new Date();
+	}
+	
+	public Event() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Date getTime() {
+		return time;
+	}
+
+	public void setTime(Date time) {
+		this.time = time;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+}
